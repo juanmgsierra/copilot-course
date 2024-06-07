4f1afd6585e406f8b967a178d9977f5367ce7e47
Juan Miguel Garcia <47489189+juanmgsierra@users.noreply.github.com>
2024-06-07 16:17:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        name: 'Member',
+        skills: ['JavaScript', 'React', 'Node'],
+        details: {
+        age: 25,
+        location: 'India'
+        }
+    }
+}    
